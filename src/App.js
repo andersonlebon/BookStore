de55e38b2d03ce9ecbcf260924004bc7fffd6409
Caleb Anderson <buyananderson@gmail.com>
2021-08-30 22:30:35 +0200

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
 import Header from './components/header';
+
 function App() {
   return (
     <Router>
       <main>
         <Header />
+        <Switch>
+          <Route path="/books" component={Books} />
+          <Route path="/categories" component={Categories} />
+        </Switch>
       </main>
     </Router>
   );
