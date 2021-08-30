dfd146c57a81bfd15e20b1b9530fdfa1dd205194
Caleb Anderson <buyananderson@gmail.com>
2021-08-30 22:29:14 +0200

    Create the Categories component

---
+++ src/components/categories.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Categories extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <h2>Under construction</h2>;
+  }
+}
+
+export default Categories;
