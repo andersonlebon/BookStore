bb33c5bf178b746f15c4e0acff733fd4b9ff33ae
Caleb Anderson <buyananderson@gmail.com>
2021-09-02 23:37:01 +0200

    Style the book informations part

--- src/components/common/book.jsx
+++ src/components/common/book.jsx
@@ -11,19 +11,23 @@ class Book extends Component {
     const { book, onDelete } = this.props;
 
     return (
-      <li className="book-item w100">
-        <div className="book-info">
-          <div>{book.title}</div>
-          <div className="book-btns">
+      <li className="book-item w100 al-center">
+        <div className="book-info w100 flex flex-col">
+          <p className="type">Action</p>
+          <p className="title">{book.title}</p>
+          <p className="auther">Caleb buyana</p>
+          <div className="book-btns flex w100 j-start">
+            <button type="button"> Comment</button>
             <button onClick={() => onDelete(book.item_id)} type="button">
-              Delete
+              Remove
             </button>
+            <button type="button"> Edite</button>
           </div>
         </div>
         <div className="book-progress  flex flex-center-all">
           <p>Completed</p>
         </div>
-        <div className="update flex flex-col al-end">
+        <div className="update">
           <p className="text-smal">CURRENT CHAPTER</p>
           <p>Chapter 17</p>
           <button type="button">UPDATE PROGRESS</button>
